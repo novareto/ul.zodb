2d22ee037b293c673f26a38a0d2650bc7f4c06f9
goschtl <ck@novareto.de>
2014-10-22 11:24:12 +0200

    add setuptools

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ version = '1.0'
 long_description = read('README.txt') + '\n' + read('HISTORY.txt')
 
 install_requires = [
+    "setuptools",
     "cromlech.zodb",
     "dolmen.container",
     "persistent",
